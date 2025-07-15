c0a85031a3f8fd2185ff8579cbd137911fffa38c
“miriyalamani” <miriyalamani@umass.edu>
2025-07-15 16:40:13 -0400

    change threshold to match for dist greater than 0.1

--- tx_extension_clip/config.py
+++ tx_extension_clip/config.py
@@ -3,7 +3,7 @@ from tx_extension_clip.hasher import CLIPHasher
 
 # Note that the embeddings must match the model used to generate the hashes.
 # This metadata must be included in the database.
-CLIP_DISTANCE_THRESHOLD: float = 0.1
+CLIP_DISTANCE_THRESHOLD: float = 1.0
 CLIP_NORMALIZED: bool = True
 OPEN_CLIP_MODEL_NAME: str = "xlm-roberta-base-ViT-B-32"
 OPEN_CLIP_PRETRAINED: str = "laion5b_s13b_b90k"

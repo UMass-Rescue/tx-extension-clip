6e726a2dc42db304009d1fa2099819b72e4e0a54
“miriyalamani” <miriyalamani@umass.edu>
2025-10-01 17:19:23 -0400

    mock libraries only for tests

--- tx_extension_clip/__init__.py
+++ tx_extension_clip/__init__.py
@@ -1,10 +1,11 @@
 import sys
+import os
 from unittest.mock import MagicMock
 
 # This file is run by the test discoverer before any tests are run.
 
 # Mock heavy dependencies for the test environment
-if "unittest" in sys.modules:
+if os.environ.get("TX_CLIP_TEST_ENV", "false").lower() == "true":
     sys.modules["torch"] = MagicMock()
     sys.modules["open_clip"] = MagicMock()
     sys.modules["open_clip.factory"] = MagicMock()

ba48ef70c699fa020388b4b096b1c2fb24fa2f2e
miriyalamani <124741271+miriyalamani@users.noreply.github.com>
2025-10-31 16:13:20 -0400

    expose float based signal to hma

--- tx_extension_clip/__init__.py
+++ tx_extension_clip/__init__.py
@@ -1,6 +1,6 @@
 from tx_extension_clip.manifest import CLIPExtensionManifest
-from tx_extension_clip.signal import CLIPSignal
+from tx_extension_clip.signal import CLIPFloatSignal
 
 TX_MANIFEST = CLIPExtensionManifest(
-    signal_types=(CLIPSignal,),
+    signal_types=(CLIPFloatSignal,),
 )

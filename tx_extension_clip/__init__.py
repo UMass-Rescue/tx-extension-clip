5fa8bd11ea90d0f763820ddd89680fe772eaa746
miriyalamani <124741271+miriyalamani@users.noreply.github.com>
2025-11-06 12:57:59 -0500

    expose hma to both clip(hamming) and clip_float signals

--- tx_extension_clip/__init__.py
+++ tx_extension_clip/__init__.py
@@ -1,6 +1,9 @@
 from tx_extension_clip.manifest import CLIPExtensionManifest
-from tx_extension_clip.signal import CLIPFloatSignal
+from tx_extension_clip.signal import CLIPFloatSignal, CLIPSignal
 
 TX_MANIFEST = CLIPExtensionManifest(
-    signal_types=(CLIPFloatSignal,),
+    signal_types=(
+        CLIPSignal,
+        CLIPFloatSignal,
+    ),
 )
